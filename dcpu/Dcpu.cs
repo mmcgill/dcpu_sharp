c31ee9cb9fe8e885be1bce57dde50f6e4861f6ff
Matt McGill <matt.mcgill@gmail.com>
2012-04-07 09:40:52 -0400

    move [G|S]etOperand, Decode to Dcpu

--- dcpu/Dcpu.cs
+++ dcpu/Dcpu.cs
@@ -8,11 +8,202 @@ namespace Com.MattMcGill.Dcpu {
             var opcode = state.Get(addr);
             state = state.Set(RegisterName.PC, (ushort)(addr + 1));
 
-            var op = Op.Decode(opcode);
+            var op = Decode(opcode);
 
             return op.Apply(state);
         }
 
-    }
+        public static Op Decode(ushort word) {
+            byte opcode = (byte)(word & 0xF);
+            if (opcode == 0) {
+                throw new NotImplementedException();
+            }
+            byte a = (byte)((word >> 4) & 0x3F);
+            byte b = (byte)((word >> 10) & 0x3F);
+            switch (opcode) {
+                case 0x01: return new Set(a, b);
+                case 0x02: return new Add(a, b);
+                case 0x03: return new Sub(a, b);
+                case 0x04: return new Mul(a, b);
+                case 0x05: return new Div(a, b);
+                default: throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Read the operand.
+        /// </summary>
+        /// <param name="operand">operand to read</param>
+        /// <param name="prev">current DCPU state</param>
+        /// <param name="next">DCPU state after reading</param>
+        /// <returns>a value</returns>
+        /// <remarks>
+        /// GetOperand modifies SP for PUSH and POP operands,
+        /// and the PC for addressing modes that read the next
+        /// word of RAM.
+        ///
+        /// We're presently assuming that getting an operand never
+        /// affects the Overflow register.
+        /// </remarks>
+        public static ushort GetOperand(byte operand, IState prev, out IState next) {
+            next = prev;
+            if (0 <= operand && operand < 8) { // register
+                return prev.Get((RegisterName) operand);
+            }
+            if (8 <= operand && operand < 16) { // [register]
+                return prev.Get(prev.Get((RegisterName) (operand & 0x07)));
+            }
+            if (16 <= operand && operand < 24) { // [next word + register]
+                var pc = (ushort) (prev.Get(RegisterName.PC) + 1);
+                var addr = (ushort) (prev.Get(pc) + prev.Get((RegisterName) (operand & 0x07)));
+                var result = prev.Get(addr);
+                next = prev.Set(RegisterName.PC, pc);
+                return result;
+            }
+            if (operand == 0x18) { // [SP++]
+                var addr = prev.Get(RegisterName.SP);
+                var result = prev.Get(addr);
+                next = prev.Set(RegisterName.SP, (ushort)(addr + 1));
+                return result;
+            }
+            if (operand == 0x19) { // [SP]
+                return prev.Get(prev.Get(RegisterName.SP));
+            }
+            if (operand == 0x1a) { // [--SP]
+                var addr = (ushort)(prev.Get(RegisterName.SP) - 1);
+                var result = prev.Get(addr);
+                next = prev.Set(RegisterName.SP, addr);
+                return result;
+            }
+            if (operand == 0x1b) { // SP
+                return prev.Get(RegisterName.SP);
+            }
+            if (operand == 0x1c) { // PC
+                return prev.Get(RegisterName.PC);
+            }
+            if (operand == 0x1d) { // O
+                return prev.Get(RegisterName.O);
+            }
+            if (operand == 0x1e) { // [next word]
+                var addr = prev.Get(RegisterName.PC);
+                var result = prev.Get(prev.Get(addr));
+                next = prev.Set(RegisterName.PC, (ushort)(addr + 1));
+                return result;
+            }
+            if (operand == 0x1f) { // next literal
+                var addr = prev.Get(RegisterName.PC);
+                var result = prev.Get(addr);
+                next = prev.Set(RegisterName.PC, (ushort)(addr + 1));
+                return result;
+            }
+            if (0x20 <= operand && operand < 0x40) { // literal
+                return (ushort)(operand - 0x20);
+            }
+            throw new ArgumentException("Invalid operand " + operand);
+        }
+
+        /// <summary>
+        /// Write the operand.
+        /// </summary>
+        /// <param name="operand">operand to write</param>
+        /// <param name="state">current DCPU state</param>
+        /// <param name="value">value to write</param>
+        /// <returns>DCPU state after writing</returns>
+        /// <remarks>
+        /// We're presently assuming that getting an operand write (other than 0x1d) never
+        /// affects the Overflow register.
+        /// </remarks>
+        public static IState SetOperand(ushort operand, ushort value, IState state) {
+            if (0x0 <= operand && operand < 0x8) { // register
+                return state.Set((RegisterName)operand, value);
+            }
+            if (0x8 <= operand && operand < 0x10) { // [register]
+                var addr = state.Get((RegisterName)(operand - 0x8));
+                return state.Set(addr, value);
+            }
+            if (0x10 <= operand && operand < 0x18) { // [next word + register]
+                var pc = state.Get(RegisterName.PC);
+                var addr = state.Get(pc);
+                var offset = state.Get((RegisterName)(operand - 0x10));
+                return state.Set(RegisterName.PC, (ushort)(pc + 1))
+                            .Set((ushort)(addr + offset), value);
+            }
+            if (operand == 0x18) { // [SP++]
+                var addr = state.Get(RegisterName.SP);
+                return state.Set(addr, value).Set(RegisterName.SP, (ushort)(addr + 1));
+            }
+            if (operand == 0x19) { // [SP]
+                return state.Set(state.Get(RegisterName.SP), value);
+            }
+            if (operand == 0x1a) { // [--SP]
+                var addr = (ushort) (state.Get(RegisterName.SP) - 1);
+                return state.Set(addr, value).Set(RegisterName.SP, addr);
+            }
+            if (operand == 0x1b) { // SP
+                return state.Set(RegisterName.SP, value);
+            }
+            if (operand == 0x1c) { // PC
+                return state.Set(RegisterName.PC, value);
+            }
+            if (operand == 0x1d) { // O
+                return state.Set(RegisterName.O, value);
+            }
+            if (operand == 0x1e) { // [next word]
+                var addr = state.Get(RegisterName.PC);
+                return state.Set(addr, value).Set(RegisterName.PC, (ushort)(addr + 1));
+            }
+            if (operand == 0x1f) { // next word
+                var addr = state.Get(RegisterName.PC);
+                return state.Set(RegisterName.PC, (ushort)(addr + 1));
+            }
+            if (0x20 <= operand && operand < 0x40) { // literal
+                var addr = state.Get (RegisterName.PC);
+                return state.Set (RegisterName.PC, (ushort)(addr + 1));
+            }
+            throw new ArgumentException("Invalid operand " + operand);
+        }
+
+        public static string OperandString( byte operand ) {
+            if (0x0 <= operand && operand < 0x8) { // register
+                return ((RegisterName)operand).ToString();
+            }
+            if (0x8 <= operand && operand < 0x10) { // [register]
+                return "[" + ((RegisterName)(operand - 0x8)).ToString() + "]";
+            }
+            if (0x10 <= operand && operand < 0x18) { // [next word + register]
+                var reg = (RegisterName)(operand - 0x10);
+                return "[" + "PC++ + " + reg + "]";
+            }
+            if (operand == 0x18) { // [SP++]
+                return "[SP++]";
+            }
+            if (operand == 0x19) { // [SP]
+                return "[SP]";
+            }
+            if (operand == 0x1a) { // [--SP]
+                return "[--SP]";
+            }
+            if (operand == 0x1b) { // SP
+                return "SP";
+            }
+            if (operand == 0x1c) { // PC
+                return "PC";
+            }
+            if (operand == 0x1d) { // O
+                return "O";
+            }
+            if (operand == 0x1e) { // [next word]
+                return "[PC++]";
+            }
+            if (operand == 0x1f) { // next word
+                return "PC++";
+            }
+            if (0x20 <= operand && operand < 0x40) { // literal
+                return ((ushort)(operand - 0x20)).ToString();
+            }
+            throw new ArgumentException("Invalid operand " + operand);
+        }
+
+     }
 }
 
--- dcpu/Tests/DcpuTest.cs
+++ dcpu/Tests/DcpuTest.cs
@@ -18,7 +18,335 @@ namespace Com.MattMcGill.Dcpu {
             Assert.AreEqual(2, state.Get (RegisterName.PC));
         }
 
-    }
+        [Test]
+        public void Decode_BasicOpcodeIsDecodedCorrectly() {
+            var op = Dcpu.Decode(0x7C01);
+            Assert.IsTrue(op is Set);
+            Assert.AreEqual(0x00, op.A);
+            Assert.AreEqual(0x1f, op.B);
+        }
 
+        [Test]
+        public void GetOperand_RegisterValue_ReturnsCorrectRegisterValue() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.A, 34);
+            IState next;
+
+            Assert.AreEqual(34, Dcpu.GetOperand(0x00, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_RegisterValue_SetsCorrectRegisterValue() {
+            var prev = new MutableState();
+            var next = Dcpu.SetOperand(0x02, 42, prev);
+
+            Assert.AreEqual(42, next.Get(RegisterName.C));
+        }
+
+        [Test]
+        public void GetOperand_IndirectRegister_ReturnsCorrectValueFromMemory() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.A, 34);
+            prev.Set(34, 42);
+            IState next;
+
+            Assert.AreEqual(42, Dcpu.GetOperand(0x08, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_IndirectRegister_SetsValueToCorrectAddress() {
+            var prev = new MutableState();
+
+            IState next = Dcpu.SetOperand(0x9, 12, prev.Set(RegisterName.B, 19));
+
+            Assert.AreEqual(12, next.Get(19));
+        }
+
+        [Test]
+        public void GetOperand_IndirectAddressPlusRegister_ReturnsCorrectValueFromMemory() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.Z, 10);
+            prev.Set(1, 10);
+            prev.Set(20, 42);
+            IState next;
+
+            Assert.AreEqual(42, Dcpu.GetOperand(21, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_IndirectAddressPlusRegister_SetValueInCorrectLocation() {
+            var prev = new MutableState()
+                .Set(RegisterName.I, 10)
+                .Set(RegisterName.PC, 0)
+                .Set((ushort)0, 15);
+
+            var next = Dcpu.SetOperand(0x16, 34, prev);
+
+            Assert.AreEqual(34, next.Get(25));
+        }
+
+        [Test]
+        public void GetOperand_IndirectAddresssPlusRegister_NextStatePCIsUpdated() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.Z, 10);
+            prev.Set(1, 10);
+            prev.Set(20, 42);
+            IState next;
+
+            Dcpu.GetOperand(21, prev, out next);
+            
+            Assert.AreEqual(1, next.Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void SetOperand_IndirectAddressPlusRegister_IncrementsPC() {
+            var next = Dcpu.SetOperand(0x16, 34, new MutableState().Set(RegisterName.PC, 0));
+
+            Assert.AreEqual(1, next.Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void GetOperand_Pop_ReturnsValueAtSPAddress() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 456);
+            prev.Set(456, 42);
+            IState next;
+
+            Assert.AreEqual(42, Dcpu.GetOperand(0x18, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_Pop_SetsValueAtSPAddress() {
+            var prev = new MutableState().Set(RegisterName.SP, 1);
+
+            var next = Dcpu.SetOperand(0x18, 42, prev);
+
+            Assert.AreEqual(42, next.Get((ushort)1));
+        }
+
+        [Test]
+        public void GetOperand_Pop_SPIsIncremented() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 456);
+            prev.Set(456, 42);
+            IState next;
+
+            Dcpu.GetOperand(0x18, prev, out next);
+
+            Assert.AreEqual(457, next.Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void SetOperand_Pop_SPIsIncremented() {
+            var state = Dcpu.SetOperand(0x18, 0, new MutableState().Set(RegisterName.SP, 1));
+            Assert.AreEqual(2, state.Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void GetOperand_Peek_ReturnsValueAtSPAddress() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 456);
+            prev.Set(456, 42);
+            IState next;
+
+            Assert.AreEqual(42, Dcpu.GetOperand(0x19, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_Peek_SetsValueAtSPAddress() {
+            var prev = new MutableState().Set(RegisterName.SP, 5);
+
+            Assert.AreEqual(10, Dcpu.SetOperand(0x19, 10, prev).Get(5));
+        }
+
+        [Test]
+        public void GetOperand_Peek_SPIsNotIncremented() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 456);
+            prev.Set(456, 42);
+            IState next;
+
+            Dcpu.GetOperand(0x19, prev, out next);
+
+            Assert.AreEqual(456, next.Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void SetOperand_Peek_SPIsNotIncremented() {
+            var prev = new MutableState().Set(RegisterName.SP, 1);
+
+            Assert.AreEqual(1, Dcpu.SetOperand(0x19, 10, prev).Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void GetOperand_Push_ReturnsValueAtOneMinusSP() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 456);
+            prev.Set(456, 10);
+            prev.Set(455, 20);
+            IState next;
+
+            Assert.AreEqual(20, Dcpu.GetOperand(0x1a, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_Push_SetsValueAtOneMinusSP() {
+            var prev = new MutableState().Set(RegisterName.SP, 10);
+
+            Assert.AreEqual(42, Dcpu.SetOperand(0x1a, 42, prev).Get(9));
+        }
+
+        [Test]
+        public void GetOperand_Push_DecrementsSP() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 100);
+            IState next;
+
+            Dcpu.GetOperand(0x1a, prev, out next);
+
+            Assert.AreEqual(99, next.Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void SetOperand_Push_DecrementsSP() {
+            var prev = new MutableState().Set(RegisterName.SP, 10);
+
+            Assert.AreEqual(9, Dcpu.SetOperand(0x1a, 1, prev).Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void GetOperand_SP_ReturnsSP() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.SP, 12);
+            IState next;
+
+            Assert.AreEqual(12, Dcpu.GetOperand(0x1b, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_SP_SetsSP() {
+            var prev = new MutableState().Set(RegisterName.SP, 10);
+
+            Assert.AreEqual(42, Dcpu.SetOperand(0x1b, 42, prev).Get(RegisterName.SP));
+        }
+
+        [Test]
+        public void GetOperand_PC_ReturnsPC() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.PC, 100);
+            IState next;
+
+            Assert.AreEqual(100, Dcpu.GetOperand(0x1c, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_PC_SetsPC() {
+            var prev = new MutableState().Set(RegisterName.PC, 10);
+
+            Assert.AreEqual(42, Dcpu.SetOperand(0x1c, 42, prev).Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void GetOperand_O_ReturnsO() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.O, 9);
+            IState next;
+
+            Assert.AreEqual(9, Dcpu.GetOperand(0x1d, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_O_SetsO() {
+            var prev = new MutableState().Set(RegisterName.O, 10);
+
+            Assert.AreEqual(42, Dcpu.SetOperand(0x1d, 42, prev).Get(RegisterName.O));
+        }
+
+        [Test]
+        public void GetOperand_NextWord_ReturnsValueAtPC() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.PC, 1);
+            prev.Set(1, 200);
+            prev.Set(200, 42);
+            IState next;
+
+            Assert.AreEqual(42, Dcpu.GetOperand(0x1e, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_NextWord_SetsValueAtAddrInPC() {
+            var prev = new MutableState().Set(RegisterName.PC, 22);
+
+            Assert.AreEqual(99, Dcpu.SetOperand(0x1e, 99, prev).Get(22));
+        }
+
+        [Test]
+        public void GetOperand_NextWord_AdvancesPC() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.PC, 0);
+            IState next;
+
+            Dcpu.GetOperand(0x1e, prev, out next);
+
+            Assert.AreEqual(1, next.Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void SetOperand_NextWord_AdvancesPC() {
+            var prev = new MutableState().Set(RegisterName.PC, 10);
+
+            Assert.AreEqual(11, Dcpu.SetOperand(0x1e, 42, prev).Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void GetOperand_NextAsLiteral_ReturnsValueOfNextWord() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.PC, 0);
+            prev.Set((ushort)0, 10);
+            IState next;
+
+            Assert.AreEqual(10, Dcpu.GetOperand(0x1f, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_NextAsLiteral_DoesNotSetValue() {
+            var prev = new MutableState().Set(RegisterName.PC, 10);
+
+            Assert.AreEqual(0, Dcpu.SetOperand(0x1f, 5, prev).Get(10));
+        }
+
+        [Test]
+        public void GetOperand_NextAsLiteral_AdvancesPC() {
+            var prev = new MutableState();
+            prev.Set(RegisterName.PC, 0);
+            IState next;
+
+            Dcpu.GetOperand(0x1f, prev, out next);
+
+            Assert.AreEqual(1, next.Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void SetOperand_NextAsLiteral_AdvancesPC() {
+            var prev = new MutableState().Set(RegisterName.PC, 10);
+
+            Assert.AreEqual(11, Dcpu.SetOperand(0x1f, 99, prev).Get(RegisterName.PC));
+        }
+
+        [Test]
+        public void GetOperand_Literal_ReturnsLiteralValue() {
+            var prev = new MutableState();
+            IState next;
+
+            Assert.AreEqual(5, Dcpu.GetOperand(0x25, prev, out next));
+        }
+
+        [Test]
+        public void SetOperand_Literal_AdvancesPC() {
+            var prev = new MutableState().Set(RegisterName.PC, 10);
+
+            Assert.AreEqual(11, Dcpu.SetOperand(0x22, 0, prev).Get(RegisterName.PC));
+        }
+    }
 }
 
