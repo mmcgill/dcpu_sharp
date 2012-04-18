d736ed902a0acdbb87bff730293d251b7bfcf5d3
Matt McGill <matt.mcgill@gmail.com>
2012-04-17 20:08:02 -0400

    add colortest.asm

---
+++ dcpu/Examples/colortest.asm
@@ -0,0 +1,24 @@
+﻿set i, 0	; index into video memory
+set j, 0	; color
+set z, 48	; character
+
+:loop
+	set a, j
+	add j, 1
+	and j, 0xff
+
+	shl a, 8
+	bor a, z
+	add z, 1
+	ifg z, 90
+		set z, 48
+
+	set [0x8000+i], a
+	add i, 1
+	ifn i, 384
+		set pc, loop
+
+:done
+	set pc, done
+
+
