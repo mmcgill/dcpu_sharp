3d2b775e66fa0ebbb5fb9c26122713a6a644b297
Matt McGill <matt.mcgill@gmail.com>
2012-04-23 22:55:40 -0400

    refactor Immutable state to use PersistentArray<ushort>

--- dcpu/ImmutableState.cs
+++ dcpu/ImmutableState.cs
@@ -3,21 +3,27 @@ using System;
 namespace Com.MattMcGill.Dcpu {
 
     public class ImmutableState : AbstractState, IState {
-        private ushort[] _memory;
-        private ushort[] _regs;
+        private PersistentArray<ushort> _memory;
+        private PersistentArray<ushort> _regs;
 
         public ImmutableState() {
-            _memory = new ushort[Dcpu.MAX_ADDRESS + 1];
-            _regs = new ushort[11];
+            _memory = new PersistentArray<ushort>(Dcpu.MAX_ADDRESS + 1);
+            _regs = new PersistentArray<ushort>(11);
         }
 
-        protected ImmutableState(ImmutableState prev) : base(prev) {
-            // Temporarily use a complete array copy until we implement
-            // a more efficient persistent data structure.
-            _memory = new ushort[Dcpu.MAX_ADDRESS + 1];
-            Array.Copy(prev._memory, _memory, Dcpu.MAX_ADDRESS + 1);
-            _regs = new ushort[11];
-            Array.Copy(prev._regs, _regs, 11);
+        protected ImmutableState(ImmutableState prev, Register register, ushort value) : base(prev) {
+            _memory = prev._memory;
+            _regs = prev._regs.Set((int)register, value);
+        }
+
+        protected ImmutableState(ImmutableState prev, ushort addr, ushort value) : base(prev) {
+            _memory = prev._memory.Set(addr, value);
+            _regs = prev._regs;
+        }
+
+        protected ImmutableState(ushort[] memory) {
+            _memory = new PersistentArray<ushort>(memory);
+            _regs = new PersistentArray<ushort>(11);
         }
 
         public ushort Get(Register reg) {
@@ -29,24 +35,16 @@ namespace Com.MattMcGill.Dcpu {
         }
 
         public IState Set(Register reg, ushort value) {
-            var next = new ImmutableState(this);
-            next._regs[(int)reg] = value;
-            return next;
+            return new ImmutableState(this, reg, value);
         }
 
         public IState Set(ushort addr, ushort value) {
             WriteToDevice(addr, value);
-
-            var next = new ImmutableState(this);
-            next._memory[addr] = value;
-            return next;
+            return new ImmutableState(this, addr, value);
         }
 
         public static ImmutableState ReadFromFile(string path) {
-            var state = new ImmutableState();
-            var image = LoadImage(path);
-            Array.Copy(image, state._memory, Dcpu.MAX_ADDRESS + 1);
-            return state;
+            return new ImmutableState(LoadImage(path));
         }
     }
 }
