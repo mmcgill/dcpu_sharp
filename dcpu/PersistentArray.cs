a1fbe025978621d72c9ff8f23e5242fd8f01bdba
Matt McGill <matt.mcgill@gmail.com>
2012-04-23 22:54:55 -0400

    implement PersistantArray<T>

---
+++ dcpu/PersistentArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.MattMcGill.Dcpu {
+    public class PersistentArray<T> {
+        private readonly int _length;
+        private readonly T _value;
+        private readonly PersistentArray<T> _left;
+        private readonly PersistentArray<T> _right;
+
+        public int Length { get { return _length; } }
+
+        public PersistentArray(int length) {
+            _length = length;
+            if (_length > 1) {
+                _left = new PersistentArray<T>((length + 1) / 2);
+                _right = new PersistentArray<T>(length - _left._length);
+            }
+        }
+
+        public PersistentArray(T[] source) : this(source, 0, source.Length) {}
+
+        public PersistentArray(T[] source, int index, int length) {
+            _length = length;
+            if (_length > 1) {
+                _left = new PersistentArray<T>(source, index, (length + 1) / 2);
+                _right = new PersistentArray<T>(source, index + _left._length, length - _left._length);
+            } else {
+                _value = source[index];
+            }
+        }
+
+        private PersistentArray(T value) {
+            _length = 1;
+            _value = value;
+        }
+
+        private PersistentArray(PersistentArray<T> left, PersistentArray<T> right) {
+            _length = left._length + right._length;
+            _left = left;
+            _right = right;
+        }
+
+        public T this[int index] {
+            get {
+                if (index >= _length)       throw new IndexOutOfRangeException();
+                if (_length == 1)           return _value;
+                if (index < _left._length)  return _left[index];
+                else                        return _right[index - _left._length];
+            }
+        }
+
+        public PersistentArray<T> Set(int index, T value) {
+            if (index >= _length)       throw new IndexOutOfRangeException();
+            if (_length == 1)           return new PersistentArray<T>(value);
+            if (index < _left._length)  return new PersistentArray<T>(_left.Set(index, value), _right);
+            else                        return new PersistentArray<T>(_left, _right.Set(index - _left._length, value));
+        }
+    }
+}
---
+++ dcpu/Tests/PersistentArrayTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Com.MattMcGill.Dcpu.Tests {
+    [TestFixture]
+    public class PersistentArrayTest {
+        [Test]
+        public void EmptyArrayHasLengthZero() {
+            Assert.AreEqual(0, new PersistentArray<string>(0).Length);
+        }
+
+        [Test]
+        public void EmptyArrayThrowsIndexOutOfRange() {
+            Assert.Throws<IndexOutOfRangeException>(() => new PersistentArray<string>(0).Set(0, "foo"));
+        }
+
+        [Test]
+        public void Set_LengthOneArray_OldValueIdentical() {
+            var array = new PersistentArray<string>(1);
+            var array2 = array.Set(0, "foo");
+            Assert.AreEqual(null, array[0]);
+        }
+
+        [Test]
+        public void Set_LengthOneArray_NewValueCorrect() {
+            var array = new PersistentArray<string>(1);
+            var array2 = array.Set(0, "foo");
+            Assert.AreEqual("foo", array2[0]);
+        }
+
+        [Test]
+        public void GetAndSet_ManyElementArray_CorrectElementsReturned() {
+            var array = new PersistentArray<int>(100);
+            for (int i=0; i < 100; ++i) {
+                array = array.Set(i, 2 * i);
+            }
+
+            for (int i=0; i < 100; ++i) {
+                Assert.AreEqual(2 * i, array[i]);
+            }
+
+            var array2 = array;
+            for (int i=0; i < 100; ++i) {
+                array2 = array2.Set(i, array[99 - i]);
+            }
+
+            for (int i=99; i >= 0; --i) {
+                Assert.AreEqual(2 * i, array2[99 - i]);
+            }
+        }
+
+        [Test]
+        public void ConstructFromSourceArray() {
+            var source = new int[60];
+            for (int i=0; i < source.Length; ++i)
+                source[i] = i * 3;
+            var array = new PersistentArray<int>(source);
+            for (int i=0; i < source.Length; ++i)
+                Assert.AreEqual(source[i], array[i]);
+        }
+    }
+}
